8e3d58971a33873dc898783ce8ebb874fc9b61ca
Alexander F. Müller <afmueller@users.noreply.github.com>
2021-02-28 00:38:56 +0100

    Clean up run script

--- runscript.bat
+++ runscript.bat
@@ -1,11 +1,14 @@
 @echo off
 echo Current Directory is %cd%
 echo Current batch run is %~dpnx0
-echo Files to rename: %*
+
+set filesC=0
+for %%f in (%*) do Set /A filesC+=1
+echo Files to rename: %filesC%
+
 echo.
 SET script_path=%~dpnx0%
 SET script_dir=%script_path:\runscript.bat=%
 echo cmd /k "cd /d %script_dir%\venv\Scripts & activate & cd /d %script_dir% & python smart_rename.py %*"
 cmd /k "cd /d %script_dir%\venv\Scripts & activate & cd /d %script_dir% & python --version & python smart_rename.py %* & deactivate & exit 0"
 pause
-REM cmd /k "cd /d C:\Users\Admin\Desktop\venv\Scripts & activate & cd /d  C:\Users\Admin\Desktop\helloworld & python manage.py runserver"
