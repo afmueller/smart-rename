941b4bf6d19c21a5360605f00969255bf9e9d347
Alexander F. Müller <afmueller@users.noreply.github.com>
2021-02-28 00:47:11 +0100

    Shorten echo of parameter list to just show first and last argument path

--- runscript.bat
+++ runscript.bat
@@ -3,12 +3,13 @@ echo Current Directory is %cd%
 echo Current batch run is %~dpnx0
 
 set filesC=0
-for %%f in (%*) do Set /A filesC+=1
+for %%f in (%*) do ( set /a filesC+=1 & set last=%%f )
 echo Files to rename: %filesC%
 
 echo.
 SET script_path=%~dpnx0%
 SET script_dir=%script_path:\runscript.bat=%
-echo cmd /k "cd /d %script_dir%\venv\Scripts & activate & cd /d %script_dir% & python smart_rename.py %*"
+echo cmd /k "cd /d %script_dir%\venv\Scripts & activate & cd /d %script_dir% & python smart_rename.py %1 ... %last%"
+echo.
 cmd /k "cd /d %script_dir%\venv\Scripts & activate & cd /d %script_dir% & python --version & python smart_rename.py %* & deactivate & exit 0"
 pause
